653dc38506fac8658060d2d5a7414ddc174f975e
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-06 18:29:08 -0300

    feat: add indicator component

---
+++ src/components/indicator.tsx
@@ -0,0 +1,9 @@
+import { View, type ViewProps } from "react-native"
+import { cn } from "@/helpers/cn"
+
+type IndicatorProps = ViewProps
+
+export function Indicator(props: IndicatorProps) {
+  const { className, ...rest } = props
+  return <View className={cn(className, "bg-lime-300 w-4 h-4 rounded-full items-center justify-center")} {...rest} />
+}
