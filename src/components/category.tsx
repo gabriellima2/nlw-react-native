78dbd42605ba772364550389f7409452e244f6da
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-06 18:44:22 -0300

    feat: add category component

---
+++ src/components/category.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/helpers/cn";
+import { Pressable, PressableProps, Text } from "react-native";
+
+export type CategoryProps = PressableProps & {
+  title: string
+  isSelected?: boolean
+}
+
+export function Category(props: CategoryProps) {
+  const { title, isSelected, className, ...rest } = props
+  return (
+    <Pressable
+      className={cn(
+        className,
+        "bg-slate-800 px-4 justify-center rounded-md h-10",
+        isSelected && 'border-2 border-lime-300'
+      )}
+      {...rest}
+    >
+      <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
+    </Pressable>
+  )
+}
