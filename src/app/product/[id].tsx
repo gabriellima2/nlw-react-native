13ca9080e5cced89c2f4b528473fab3a01d3fa49
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-08 16:37:06 -0300

    fix: render product title in product-details screen

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -32,6 +32,7 @@ export default function Product() {
     <View className="flex-1">
       <Image source={product.cover} resizeMode="cover" className="w-full h-52" />
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text
           className="text-lime-400 text-2xl font-heading my-2"
         >
