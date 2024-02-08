ec5992f5f80c8c277d4fe691209178b5a8cc9da8
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-02-08 18:32:47 -0300

    feat: persist the cart state in local-storage using async-storage and zustand-middleware

--- src/store/cart-store/cart.store.ts
+++ src/store/cart-store/cart.store.ts
@@ -1,40 +1,47 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import type { CartStoreProperties } from "./@types/cart-store-properties";
 import type { Product } from "./@types/product";
 import type { ProductProps } from "@/data/products";
 
-export const useCartStore = create<CartStoreProperties>((set, get) => ({
-  products: [] as Product[],
-  add: (product: ProductProps) => {
-    const { products } = get()
-    const alreadyAdded = products.some(({ id }) => product.id === id)
-    if (!alreadyAdded) {
-      return set((state) => ({ ...state, products: [ ...products, { ...product, quantity: 1 } ] }))
-    }
-    const updatedProducts = products.map((cartProduct) => {
-      if (cartProduct.id === product.id) {
-        return { ...cartProduct, quantity: cartProduct.quantity + 1 }
+export const useCartStore = create(
+  persist<CartStoreProperties>((set, get) => ({
+    products: [] as Product[],
+    add: (product: ProductProps) => {
+      const { products } = get()
+      const alreadyAdded = products.some(({ id }) => product.id === id)
+      if (!alreadyAdded) {
+        return set((state) => ({ ...state, products: [ ...products, { ...product, quantity: 1 } ] }))
       }
-      return cartProduct
-    })
-    set((state) => ({ ...state, products: updatedProducts }))
-  },
-  remove: (id: string) => {
-    const { products } = get()
-    const productsWithUpdatedQuantity = products.map((product) => {
-      if (product.id !== id) return product
-      return { ...product, quantity: !!product.quantity ? product.quantity - 1 : 0 }
-    })
-    const updatedProducts = productsWithUpdatedQuantity.filter((product) => !!product.quantity)
-    set((state) => ({ ...state, products: updatedProducts }))
-  },
-  calcAmount: () => {
-    const { products } = get()
-    return products.reduce((amount, product) => amount + product.quantity, 0)
-  },
-  calcTotal: () => {
-    const { products } = get()
-    return products.reduce((total, product) => total + product.price * product.quantity, 0)
-  }
-}))
+      const updatedProducts = products.map((cartProduct) => {
+        if (cartProduct.id === product.id) {
+          return { ...cartProduct, quantity: cartProduct.quantity + 1 }
+        }
+        return cartProduct
+      })
+      set((state) => ({ ...state, products: updatedProducts }))
+    },
+    remove: (id: string) => {
+      const { products } = get()
+      const productsWithUpdatedQuantity = products.map((product) => {
+        if (product.id !== id) return product
+        return { ...product, quantity: !!product.quantity ? product.quantity - 1 : 0 }
+      })
+      const updatedProducts = productsWithUpdatedQuantity.filter((product) => !!product.quantity)
+      set((state) => ({ ...state, products: updatedProducts }))
+    },
+    calcAmount: () => {
+      const { products } = get()
+      return products.reduce((amount, product) => amount + product.quantity, 0)
+    },
+    calcTotal: () => {
+      const { products } = get()
+      return products.reduce((total, product) => total + product.price * product.quantity, 0)
+    }
+  }), {
+    name: 'nlw-expert:cart',
+    storage: createJSONStorage(() => AsyncStorage)
+  })
+)
